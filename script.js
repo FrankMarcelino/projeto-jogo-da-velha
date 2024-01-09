4f43c563926036b857f9c78fc52830c4ef0729fa
frankMarcelino <frank.silva.ti@gmail.com>
2024-01-09 09:16:45 -0300

    first commit do dia

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const todasAsCelulas = document.querySelectorAll('.celula')
+const containerJogo = document.querySelector('.container-jogo')
 
-let vezDoCirculo;
+let vezDoCirculo = false;
 
-const marcado  = (celula, adicionaClasse) => {
-    celula.classList.adda(adicionaClasse)
+const marca  = (celula, adicionaClasse) => {
+    celula.classList.add(adicionaClasse)
 } 
+
+const trocaVez = () => {
+    vezDoCirculo = !vezDoCirculo
+
+    containerJogo.classList.remove('jogador0')
+    containerJogo.classList.remove('jogadorX')
+
+    if (vezDoCirculo) {
+        containerJogo.classList.add('jogador0')
+    } else {
+        containerJogo.classList.add('jogadorX')
+    }
+}
 
 const cliqueMouse = (elemento) => {
     // aplicar marca (x ou 0)
@@ -12,12 +26,13 @@ const cliqueMouse = (elemento) => {
     const adicionaClasse = vezDoCirculo ? 'jogador0' : 'jogadorX'
 
     celula.classList.add(adicionaClasse)
-    marcado(celula, )
+    marca(celula, adicionaClasse)
     // Checar por vitória
 
     // verificar por empate
 
     // mudar o simbolo
+    trocaVez()
 }
 
 for (const celula of todasAsCelulas) {
